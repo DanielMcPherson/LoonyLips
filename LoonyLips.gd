0f8460e02824dc9d1dd404fbbad5645aa10c4e35
Daniel McPherson <mcphersond@lunainc.com>
2018-07-08 16:33:16 -0400

    Multiple random stories

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,15 +1,28 @@
 extends Node2D
 
-# Prompts for player words to be used in the story
-var prompts = [ "your name", "a place", "a living thing", "a color", "a food", "a piece of clothing", "a body part" ]
 # Words that player enters to match the prompts 
 var words = []
-# Story that words will be inserted into
-var story = "%s's story:\n\nI went to %s with a %s. We ate %s %s and I wore a %s on my %s."
 # Introduction text
 var introduction = "Welcome to Loony Lips!\nPlease enter your name to start."
+var template = [ 
+	{
+	"prompts": [ "your name", "a place", "a living thing", "a color", "a food", "a piece of clothing", "a body part" ],
+	"story": "%s's story:\n\nI went to %s with a %s. We ate %s %s and I wore a %s on my %s."
+	},
+	{
+	"prompts": [ "your name", "a color", "a material", "a name", "an animal", "a feeling", "a color", "a food", "a body part" ],
+	"story": "%s's space story:\n\nI was hurtling through space on a %s rocket made of %s with my copilot %s, an artificially intelligent %s. I was feeling %s until an alarm blared. We were about to crash into a %s asteroid made of %s! A piece of the asteroid smashed into the side of our ship, knocking me out of my seat. Fortunately, I was able to activate the thrusters with my %s, steering us out of the way just in the nick of time."
+	},
+	{
+	"prompts": [ "your name", "a feeling", "a noun", "a place", "a color", "an appliance", "a body part", "a smell", "a piece of furniture" ],
+	"story": "%s's story:\n\nI was feeling %s because my favorite %s was missing. I checked in the %s, the %s %s, and even under my %s. Finally I smelled something %s. I followed my nose and found that it was in my %s all along."
+	}
+]
+var current_story
 	
 func _ready():
+	randomize();
+	current_story = template[randi() % template.size()]
 	$Blackboard/TextBox.clear()
 	$Blackboard/OKButton/RichTextLabel.text = "OK!"
 	$Blackboard/TextBox.grab_focus()
@@ -28,7 +41,7 @@ func _on_TextBox_text_entered(new_text):
 	handle_display()
 
 func is_story_done():
-	return words.size() >= prompts.size()
+	return words.size() >= current_story.prompts.size()
 	
 func handle_display():
 	if words.size() == 0:
@@ -43,10 +56,10 @@ func show_introduction():
 	$Blackboard/StoryText.text = introduction
 	
 func prompt_player():
-	$Blackboard/StoryText.text = "Please enter " + prompts[words.size()]
+	$Blackboard/StoryText.text = "Please enter " + current_story.prompts[words.size()]
 	
 func tell_story():
-	$Blackboard/StoryText.text = story % words
+	$Blackboard/StoryText.text = current_story.story % words
 	end_game()
 	
 func end_game():
